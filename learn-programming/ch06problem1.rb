012e6eff47b766f08d26839e8f85f68c4d02a04d
Jake Stewart <stewart.b.jacob@gmail.com>
2015-05-06 10:04:36 -0500

    Add Solution to CH 6 Problem 1 - 99 Bottles of Beer

---
+++ learn-programming/ch06problem1.rb
@@ -0,0 +1,27 @@
+#99 Bottles of Beer on the Wall
+
+beer = 99
+
+while beer >= 1
+	puts beer.to_s + ' bottles of beer on the wall!'
+	puts beer.to_s + ' bottles of beer!'
+	beer = beer - 1
+	puts 'take one down, pass it around'
+
+	if beer == 1
+		puts beer.to_s + ' bottle of beer on the wall!'
+	else
+		puts beer.to_s + ' bottles of beer on the wall!'
+	end
+
+	puts ''
+
+	if beer == 1
+		puts beer.to_s + ' bottle of beer on the wall!'
+		puts beer.to_s + ' bottle of beer!'
+		beer = beer - 1
+		puts 'take one down, pass it around'
+		puts beer.to_s + ' bottles of beer on the wall!'
+	end
+end
+puts 'Now you\'re trashed!'
